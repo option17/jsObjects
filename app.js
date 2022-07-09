271844aa0d7d15e9d71db7acd93054aa2a19143d
Your Name <you@example.com>
2022-07-09 15:11:16 -0600

    making progress

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ let company ={
     yearlyRevenue : 500000,
     ceo : `ollie`,
     numberOfEmployees : 47,
-
+    managers : [`shaq`, `bob`, `tanya`],
 };
+
 
 if (company[`yearlyRevenue`] >= 1000000){
     console.log (`Way to go ${company[`name`]},${company[`ceo`]}, ${company[`numberOfEmployees`]}, employees! `);
-} else {
-    console.log("Do better!")
+} else { console.log (`do better!`);
+    
 }
+let counter = 0;
+while(counter < company[`managers`].length) {
+    console.log(company[`managers`][counter]);
+    counter = counter + 1;
+}
+
