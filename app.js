fa3ca6802b71066151f148cf4a18f79f05c4f6a3
Your Name <you@example.com>
2022-07-09 13:57:37 -0600

    Finished KC

---
+++ app.js
@@ -0,0 +1,13 @@
+let company ={
+    name :`chev`,
+    yearlyRevenue : 500000,
+    ceo : ollie,
+    numberOfEmployees : 47,
+
+};
+
+if (company[`yearlyRevenue`] >= 1000000){
+    console.log (`Way to go ${company[`name`]},${company[`ceo`]}, ${company[`numberOfEmployees`]}, employees! `);
+} else {
+    console.log("Do better!")
+}
